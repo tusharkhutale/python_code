f1f2223c21facef17979c49ad9682d96a52c9378
tusharkhutale <57614382+tusharkhutale@users.noreply.github.com>
2021-08-21 20:52:57 +0530

    Add files via upload

---
+++ whatsupAutomation.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 21 20:35:12 2021
+
+@author: Tushar
+"""
+
+import pywhatkit
+pywhatkit.sendwhatmsg('+91 9823658677', 'Happy Birthday Sweet Heart', 20,42)
